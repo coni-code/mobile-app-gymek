919380f54ca49f51fe34270aad62ad2ca81ac8dd
KiviGibson <poplawski143@gmail.com>
2023-10-25 19:50:15 +0000

    Add hook and display on screen result of hook operation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import useFetch from './utils/useFetch';
+import data from "./api/endpoint.json"
 export default function App() {
+  const [val, callUrl] = useFetch({url:data.baseApiUrl+data.getRegistrationForm.url,method:data.getRegistrationForm.method[0]});
+  function newCall(){
+    callUrl({})
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{val}</Text>
       <StatusBar style="auto" />
     </View>
   );
