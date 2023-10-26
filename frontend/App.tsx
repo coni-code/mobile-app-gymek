693db5b67a919bb8445b7d97d48064cb44bc7893
KiviGibson <poplawski143@gmail.com>
2023-10-26 20:01:37 +0000

    clean mess from test code

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import useFetch from './utils/useFetch';
-import data from "./api/endpoint.json"
 export default function App() {
-  const [val, callUrl] = useFetch({url:data.baseApiUrl+data.getRegistrationForm.url,method:data.getRegistrationForm.method[0]});
-  function newCall(){
-    callUrl({})
-  }
   return (
     <View style={styles.container}>
-      <Text>{val}</Text>
+      <Text>App.tsx</Text>
       <StatusBar style="auto" />
     </View>
   );
