9fb6b3a0283ea841ce849889a278afa8f2c8a41a
KiviGibson <poplawski143@gmail.com>
2023-10-29 14:19:19 +0000

    changed value in initialization to optional

--- frontend/utils/useLocalStorage.tsx
+++ frontend/utils/useLocalStorage.tsx
@@ -1,21 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 
-const useLocalStorage = (itemName:string,value:object|null) => {
+const useLocalStorage = (itemName:string,value?:object) => {
     const [name, setName] = useState(itemName)
 
-    async function SetItem(value:object): Promise<object>
+    async function setItem(value:object): Promise<object>
     {
         try{
             await AsyncStorage.setItem(name,JSON.stringify(value));
             return {"success":"Success"}
         }
-        catch{
+        catch (err:any)
+        {
             return {"error":500}
         }
     }
 
-    async function GetItem(): Promise<object>
+    async function getItem(): Promise<object>
     {
         try{
             const data = await AsyncStorage.getItem(name)
@@ -29,11 +30,12 @@ const useLocalStorage = (itemName:string,value:object|null) => {
     }
 
     useEffect(()=>{
-        if(value !== null)
-            SetItem(value)
+        let a
+        if(value !== undefined)
+            a = setItem(value)
     },[])
-
-    return [SetItem, GetItem]
+    const storage = {setItem, getItem}
+    return storage
 }
 
 export default useLocalStorage;
