3fa95808ff1eed2678dbaf5fe4810bc78f7793c8
KiviGibson <poplawski143@gmail.com>
2023-11-02 17:06:45 +0000

    delete zombie code

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./components/StartUp"
-import { useEffect, useState } from 'react';
-import useLocalStorage from './utils/useLocalStorage';
+import { useState } from 'react';
 export default function App() {
   const [startup, setStartUp] = useState(false)
 
   function setdone(success:boolean){
     setStartUp(success)
   }
-  
+
   if(!startup)
     return(
       <View style={styles.container}>
