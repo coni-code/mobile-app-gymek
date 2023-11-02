0d2500efd886b560b3babe23dfe5d8fa6453b1ab
KiviGibson <poplawski143@gmail.com>
2023-11-02 17:04:39 +0000

    add functionality to startup app before run

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./components/StartUp"
+import { useEffect, useState } from 'react';
+import useLocalStorage from './utils/useLocalStorage';
 export default function App() {
+  const [startup, setStartUp] = useState(false)
+
+  function setdone(success:boolean){
+    setStartUp(success)
+  }
+  
+  if(!startup)
+    return(
+      <View style={styles.container}>
+        <Loading setdone={setdone}></Loading>
+      </View>
+    );
   return (
     <View style={styles.container}>
-      <Text>App.tsx</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
