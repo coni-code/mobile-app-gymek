3416fdbc1b9967561bdbdfc2cc408f9f82eae739
KiviGibson <poplawski143@gmail.com>
2023-11-10 19:23:38 +0000

    make screen render properly with capability to edit form elements value and store it in hook

--- frontend/screens/Registration.tsx
+++ frontend/screens/Registration.tsx
@@ -1,41 +1,109 @@
-import React, { useEffect, useState } from "react"
-import useApi from "../utils/useApi"
-import { StyleSheet ,TextInput, SafeAreaView } from "react-native"
+import React, { useEffect, useState } from "react";
+import useApi from "../utils/useApi";
+import { StyleSheet, TextInput, SafeAreaView, Button } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
-const Registration = () => 
-{
-    const [data,Api] = useApi()
-    const [form, setForm] = useState({})
-    const [elements, setElements] = useState<React.JSX.Element[]>([])
-    useEffect(()=>{
-        Api.get("getRegistrationForm")
-    },[])
-    useEffect(()=>{
-        const form = data as Form
-        let keys = []
-        for(const element in form){
-            keys.push(element)
-        }
-        setForm(form)
-        const elements = keys.map((e)=><TextInput style={styles.input} placeholder={e} key={Math.random()} value={form[e].value} onChange={(event)=>onChange(event,e)}></TextInput>) as React.JSX.Element[]
-        setElements(elements)
-    },[data])
-    function onChange(event:any, name:string){
-        const value = event.target.value
-        console.log(name, value)
+const Registration = () => {
+  const [data, Api] = useApi();
+  const [form, setForm] = useState<Form>({});
+  const [elements, setElements] = useState<React.JSX.Element[]>([]);
+  useEffect(() => {
+    Api.get("getRegistrationForm");
+  }, []);
+
+  useEffect(() => {
+    const form = data as Form;
+    setForm(form);
+  }, [data]);
+
+  useEffect(() => {
+    renderForm(getKey(form));
+  }, [form]);
+
+  function onChange(event: any, name: string) {
+    const value = event;
+    setForm((past) => {
+      return {
+        ...past,
+        [name]: {
+          type: past[name].type,
+          value: value,
+          choices: past[name].choices,
+        },
+      };
+    });
+  }
+
+  function renderForm(keys: string[]) {
+    const elements = keys.map((e) => {
+      let element = <></>;
+      let type = form[e].type;
+      let password = false;
+      switch (type) {
+        case "password":
+          password = true;
+        case "email":
+        case "text":
+          element = (
+            <TextInput
+              key={e}
+              style={styles.input}
+              placeholder={e}
+              onChange={(event) => onChange(event.nativeEvent.text, e)}
+              value={form[e].value}
+              secureTextEntry={password}
+            />
+          );
+          break;
+        case "choice":
+          const options = getKey(form[e].choices).map((e) => (
+            <Picker.Item key={e} label={e} value={e} />
+          ));
+          element = (
+            <Picker
+              selectedValue={form[e].value}
+              onValueChange={(item) => onChange(item, e)}
+              mode="dropdown"
+            >
+              {options}
+            </Picker>
+          );
+          break;
+      }
+      return element;
+    }) as React.JSX.Element[];
+    setElements(elements);
+  }
+
+  function getKey(form: Form | object | undefined) {
+    if (undefined) return [];
+    let keys = [];
+    for (const element in form) {
+      keys.push(element);
     }
-    return(
-        <SafeAreaView>
-            {elements}
-        </SafeAreaView>
-    )
-    
-}
+    return keys;
+  }
+
+  function handleSubmit() {
+    // validate
+
+    //send 
+  }
+
+  return (
+    <SafeAreaView>
+      {elements}
+      <Button title={"Register"} onPress={handleSubmit}/>
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },});
-export default Registration
+  input: {
+    height: 32,
+    margin: 3,
+    borderWidth: 1,
+    minWidth: 200,
+    padding: 12,
+  },
+});
+export default Registration;
