ea674e947b6c4a3745576e28aef71bd3b8b759af
bartek3xe <bartek3xe@gmail.com>
2023-11-10 18:21:39 +0100

    Add configuration for mailer and validator

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
