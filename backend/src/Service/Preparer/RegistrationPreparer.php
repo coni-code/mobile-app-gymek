b671cbd603172f25a4b1f3babe7e35fc8f4eb11e
bartek3xe <bartek3xe@gmail.com>
2023-11-11 18:49:13 +0100

    Remove unnecessary if statements and hash password

--- backend/src/Service/Preparer/RegistrationPreparer.php
+++ backend/src/Service/Preparer/RegistrationPreparer.php
@@ -3,9 +3,14 @@
 namespace App\Service\Preparer;
 
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationPreparer
 {
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public function prepare(array $data, User $user): User
     {
         [
@@ -18,13 +23,14 @@ class RegistrationPreparer
             'gender'      => $gender,
         ] = $data;
 
-        $user->getName() !== $name['value'] && $user->setName($name['value']);
-        $user->getSurname() !== $surname['value'] && $user->setSurname($surname['value']);
-        $user->getEmail() !== $email['value'] && $user->setEmail($email['value']);
-        $user->getPassword() !== $password['value'] && $user->setPassword($password['value']);
-        $user->getPhoneNumber() !== $phoneNumber['value'] && $user->setPhoneNumber($phoneNumber['value']);
-        $user->getCity() !== $city['value'] && $user->setCity($city['value']);
-        $user->getGender() !== $gender['value'] && $user->setGender($gender['value']);
+        $user->setName($name['value']);
+        $user->setSurname($surname['value']);
+        $user->setEmail($email['value']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password['value']));
+        $user->setPhoneNumber($phoneNumber['value']);
+        $user->setCity($city['value']);
+        $user->setGender($gender['value']);
+        $user->setRoles(['ROLE_USER']);
 
         return $user;
     }
