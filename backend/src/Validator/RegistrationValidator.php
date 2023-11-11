38ab53dad20fa1a8624f0f4b6f7f6914893277b3
bartek3xe <bartek3xe@gmail.com>
2023-11-11 18:47:46 +0100

    Prepare validator to work properly, add conditions

--- backend/src/Validator/RegistrationValidator.php
+++ backend/src/Validator/RegistrationValidator.php
@@ -2,11 +2,16 @@
 
 namespace App\Validator;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class RegistrationValidator extends ConstraintValidator
 {
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
     public function validate(mixed $value, Constraint $constraint)
     {
         if (null === $value || '' === $value) {
@@ -31,13 +36,13 @@ class RegistrationValidator extends ConstraintValidator
         $city        = $city['value'];
         $gender      = $gender['value'];
 
-        if (empty($name) || !ctype_alpha($name)) {
+        if (empty($name) || preg_match('/\d/', $name)) {
             $this->context->buildViolation('Invalid name.')
                 ->atPath('name')
                 ->addViolation();
         }
 
-        if (empty($surname) || !ctype_alpha($surname)) {
+        if (empty($surname) || preg_match('/\d/', $surname)) {
             $this->context->buildViolation('Invalid surname.')
                 ->atPath('surname')
                 ->addViolation();
@@ -49,25 +54,39 @@ class RegistrationValidator extends ConstraintValidator
                 ->addViolation();
         }
 
+        if ($this->userRepository->findOneBy(['email' => $email])) {
+            $this->context->buildViolation('Account with this email is already taken.')
+                ->atPath('email')
+                ->addViolation();
+        }
+
+        if ($this->userRepository->findOneBy(['phoneNumber' => $phoneNumber])) {
+            $this->context->buildViolation('Account with this phone number is already taken.')
+                ->atPath('phoneNumber')
+                ->addViolation();
+        }
+
         if (empty($password) || strlen($password) < 6) {
             $this->context->buildViolation('Password must be at least 6 characters long.')
                 ->atPath('password')
                 ->addViolation();
         }
 
-        if (empty($phoneNumber) || !ctype_digit($phoneNumber)) {
-            $this->context->buildViolation('Invalid phone number.')
-                ->atPath('phoneNumber')
+        if (!preg_match('/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[-_!@#$%^&*()+]).*$/', $password)) {
+            $this->context->buildViolation(
+                'Password must include at least: 1 upper case letter, 1 lower case letter, 1 number, 1 special character.'
+            )
+                ->atPath('password')
                 ->addViolation();
         }
 
-        if (empty($city)) {
-            $this->context->buildViolation('City cannot be empty.')
+        if (empty($city) || preg_match('/\d/', $city)) {
+            $this->context->buildViolation('Invalid city.')
                 ->atPath('city')
                 ->addViolation();
         }
 
-        if (empty($gender) || !in_array($gender, ['male', 'female'])) {
+        if (empty($gender) || !in_array($gender, ['male', 'female', 'other'])) {
             $this->context->buildViolation('Invalid gender.')
                 ->atPath('gender')
                 ->addViolation();
