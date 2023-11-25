c363ee6f43be6a3d43a63edf0c771664d772f5b0
bartek3xe <bartek3xe@gmail.com>
2023-11-25 12:10:17 +0100

    Create links in login and registration form

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,10 +1,33 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import Form from "../components/Form";
-const Login = () => {
-  return (
-    <SafeAreaView>
-      <Form name={"loginForm"} action={"loginUser"} prompt={"Login"} ico={"arrow-right"}/>
-    </SafeAreaView>
-  );
-};
+import {ReactElement} from "react";
+
+function Login (props: { navigation: { navigate: (arg0: string) => void; }; }): ReactElement {
+    const pressHandler = () => {
+        props.navigation.navigate('Registration')
+    }
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Form name={"loginForm"} action={"loginUser"} prompt={"Login"} ico={"arrow-right"}/>
+                <TouchableOpacity style={styles.bottomLink}>
+                    <Text onPress={pressHandler}>I do not have an account</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+    },
+    bottomLink: {
+        marginTop: 50
+    }
+})
+
 export default Login;
