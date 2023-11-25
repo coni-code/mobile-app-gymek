5b0a8ef4d9cdc0ef1cd4f9acbbbe879fd2f57254
bartek3xe <bartek3xe@gmail.com>
2023-11-25 12:09:58 +0100

    Use AuthStack instead of login and refactor code

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import StartUp from "./src/screens/StartUp"
 import { PaperProvider, MD3LightTheme as DefaultTheme } from 'react-native-paper';
+import AuthStack from "./src/navigation/AuthStack";
+import {NavigationContainer} from "@react-navigation/native";
 
 const theme = {
     ...DefaultTheme,
@@ -12,7 +14,6 @@ const theme = {
 };
 
 export default function App() {
-
     const [startup, setStartUp] = useState(false)
     const [logged, setLogged] = useState(false)
 
@@ -20,21 +21,31 @@ export default function App() {
         setStartUp(success)
     }
 
-  return (
-      <PaperProvider theme={theme}>
-        <View style={styles.container}>
-          {logged?<></>:<Registration></Registration>}
-        </View>
-      </PaperProvider>
-    
-  );
+    if(!startup) {
+        return(
+            <View style={styles.container}>
+                <View style={styles.container}>
+                    <StartUp setdone={setDone}></StartUp>
+                </View>
+            </View>
+        );
+    }
+
+    return (
+        <NavigationContainer>
+            <PaperProvider theme={theme}>
+                <View style={styles.container}>
+                    {logged ? <></> : <AuthStack />}
+                </View>
+            </PaperProvider>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
 });
