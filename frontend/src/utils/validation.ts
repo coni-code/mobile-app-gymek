4162077f4556a36469179f03592f6115b468f16f
KiviGibson <poplawski143@gmail.com>
2023-11-29 19:37:47 +0000

    add function to dynamicly validate form values

---
+++ frontend/src/utils/validation.ts
@@ -0,0 +1,17 @@
+export default function validate(text:string, pattern:string): errortype{
+    if(!text) return errortype.notPassed
+    const reg = new RegExp(pattern,'g')
+    console.log(text.match(reg))
+    if(text.match(reg)?.length == 1){
+            return errortype.good
+    }
+    else{
+        return errortype.invalidValue
+    }
+}
+export enum errortype{
+    "good",
+    "notPassed",
+    "tooShort",
+    "invalidValue"
+}
