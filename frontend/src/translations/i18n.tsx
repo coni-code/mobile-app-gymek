15698e207439ab9d85d7b775dcedbb4d5928e027
bartek3xe <bartek3xe@gmail.com>
2023-12-01 20:00:11 +0100

    Create i18n module and translations for pl and en

---
+++ frontend/src/translations/i18n.tsx
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import AsyncStoragePlugin from 'i18next-react-native-async-storage';
+
+import en from './locale/en.json';
+import pl from './locale/pl.json';
+
+const resources = {
+    en: { translation: en },
+    pl: { translation: pl },
+};
+
+const AsyncStorage = AsyncStoragePlugin();
+
+i18n
+    .use(AsyncStorage)
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: 'pl',
+        fallbackLng: 'pl',
+        keySeparator: false,
+        interpolation: {
+            escapeValue: false,
+        },
+        react: {
+            useSuspense: false,
+        },
+    });
+
+export default i18n;
